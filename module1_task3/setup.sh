a65f9f3b27089377673cfc7f641ff0aa44caaf00
NehemieMbg <nehemie.mbg@gmail.com>
2023-01-06 14:32:31 +0100

    hotFix

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# spawn a sandboxed environment (Ubuntu 18.04)
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+
 # Installation of Go-Hugo and Make command
-apt-get update && apt-get install -y hugo make wget
-# fixing the recipe for 'build' target fail
-# getting latest release of 'hugo'
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
-# install it
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb	
+apt-get update && apt-get install -y hugo make
+
 # Running command build to build the website
 make build
