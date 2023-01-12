40e6392a115ef9452cf07f2ccf14df77b91b4207
NehemieMbg <nehemie.mbg@gmail.com>
2023-01-12 17:08:32 +0100

    removed "make built" from setup.sg

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -7,5 +7,3 @@ apt-get update && apt-get install -y hugo make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb	
 # install it
 dpkg -i hugo_extended_0.109.0_linux-amd64.deb	
-# Running command build to build the website
-make build
