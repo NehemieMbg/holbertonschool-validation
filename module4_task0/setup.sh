ce2e6121b39312b923def6ede13313c3bc3a683d
NehemieMbg <nehemie.mbg@gmail.com>
2023-01-16 07:37:49 +0100

    Building Inside a Docker Container

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-rm -rf /usr/local/go
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt-get install ./install_hugo.deb
-npm install -g markdownlint-cli
 
+# Update package manager
+apt-get update -y
+
+# Install other dependencies
+apt-get install -y hugo
+
+# Remove Golang
+apt-get remove -y golang
